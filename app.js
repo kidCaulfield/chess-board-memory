333bfbb96aaa240ed4aa403a4f9069a122dd7e18
kidCaulfield <wesley.coderre@gmail.com>
2023-02-14 12:34:36 -0800

    initialized json api, created filterHeaders function

--- app.js
+++ app.js
@@ -2,10 +2,17 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const logger = require("morgan");
+const filterHeaders = require("./routes/services/filterHeaders");
 
+//////////// MIDDLEWARE //////////////
 app.use(logger("common"));
+app.use(express.urlencoded({ extended: true })); //middleware for parsing urlencoded data
+app.use(express.json()); // middleware for parsing incoming json
+//////////////////////////////////////
 
-app.use("/api/v1", require("./routes"));
+/////////////// ROUTES ///////////////
+app.use("/api/v1", filterHeaders, require("./routes"));
+//////////////////////////////////////
 
 app.listen(port, () => {
   console.log(`Server listening on port ${3000}`);
